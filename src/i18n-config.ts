d6aa8d3c848c8fb83ad4ce3cc8fc0af859f4e6ea
0xShin0221 <sintaronettt@gmail.com>
2024-11-18 18:22:35 +0900

    style(About.tsx): refactor section element to a single line for improved readability feat(i18n-config.ts): add i18n configuration for internationalization support in the application

---
+++ src/i18n-config.ts
@@ -0,0 +1,33 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
+import { initReactI18next } from "react-i18next";
+
+i18n
+  .use(LanguageDetector)
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    backend: {
+      // for all available options read the backend's repository readme file
+      loadPath: "/i18n/{{lng}}/{{ns}}.json",
+    },
+    fallbackLng: {
+      default: ["en"],
+    },
+
+    returnEmptyString: false,
+
+    // have a common namespace used around the full app
+    ns: ["common"],
+    defaultNS: "common",
+
+    // keySeparator: false, // we use content as keys
+
+    interpolation: {
+      escapeValue: false,
+    },
+    debug: true,
+  });
+
+export default i18n;
