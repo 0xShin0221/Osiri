5cf6acfa35eac3579e076b5316b17a957d3920bd
0xShin0221 <sintaronettt@gmail.com>
2024-12-18 18:33:09 +0900

    chore(deploy.yml): remove unused environment variables for Supabase deployment to simplify configuration chore(migrations): delete obsolete migration files related to feed collector setup to clean up the repository

--- supabase/migrations/20241218092448_setup_env_variables.sql
+++
@@ -1,6 +0,0 @@
-do $$
-begin
-  execute format('alter database %I set app.base_url = %L', current_database(), current_setting('app.base_url'));
-  execute format('alter database %I set app.service_role_key = %L', current_database(), current_setting('app.service_role_key'));
-end;
-$$ language plpgsql;
